48a485ceab7409b0e4b196422a8302eda302e2d0
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-29 23:44:07 +0900

    connect database

--- server/main.go
+++ server/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	db, err := gorm.Open(postgres.Open("host=postgres port=5432 user=postgres password=postgres dbname=local_db sslmode=disable TimeZone=Asia/Tokyo"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	fmt.Println(db)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
