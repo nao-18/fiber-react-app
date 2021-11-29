636362b08d0fdd9deed6019c3ae8532141437001
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-30 00:08:58 +0900

    add authController.go

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,9 +1,10 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nao-18/golang-and-react-app/controllers"
+)
 
 func Setup(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Get("/", controllers.Hello)
 }
