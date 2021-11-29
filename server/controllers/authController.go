470754e106d4f59aace4d8201b6552674dfc6300
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-30 01:26:14 +0900

    add response json

--- server/controllers/authController.go
+++ server/controllers/authController.go
@@ -1,7 +1,14 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nao-18/golang-and-react-app/models"
+)
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+func Register(c *fiber.Ctx) error {
+	user := models.User{
+		FirstName: "john",
+	}
+
+	return c.JSON(user)
 }
