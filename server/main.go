efc4bc94a5a713d236a29c446eab0e711bfd0a2f
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-04 16:47:10 +0900

    add api to get user

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/nao-18/golang-and-react-app/database"
 	"github.com/nao-18/golang-and-react-app/routes"
 )
@@ -10,6 +11,10 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 	app.Listen(":9000")
