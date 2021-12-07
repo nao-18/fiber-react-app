02f4ba91ce6838115aaddb6d9f63fd959970dea5
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-07 21:49:28 +0900

    added paginate

--- server/controllers/userController.go
+++ server/controllers/userController.go
@@ -9,11 +9,26 @@ import (
 )
 
 func AllUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+
+	limit := 5
+	offset := (page - 1) * limit
+	var total int64
+
 	var users []models.User
 
-	database.DB.Preload("Role").Find(&users)
+	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&users)
 
-	return c.JSON(users)
+	database.DB.Model(&models.User{}).Count(&total)
+
+	return c.JSON(fiber.Map{
+		"data": users,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": float64(int(total) / limit),
+		},
+	})
 }
 
 func CreateUser(c *fiber.Ctx) error {
