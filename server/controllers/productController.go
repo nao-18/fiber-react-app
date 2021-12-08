c173f867557961fab09ae378d5b8d178a74d8a90
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-08 20:59:15 +0900

    added interface

--- server/controllers/productController.go
+++ server/controllers/productController.go
@@ -11,24 +11,7 @@ import (
 func AllProducts(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
 
-	limit := 5
-	offset := (page - 1) * limit
-	var total int64
-
-	var products []models.Product
-
-	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&products)
-
-	database.DB.Model(&models.Product{}).Count(&total)
-
-	return c.JSON(fiber.Map{
-		"data": products,
-		"meta": fiber.Map{
-			"total":     total,
-			"page":      page,
-			"last_page": float64(int(total) / limit),
-		},
-	})
+	return c.JSON(models.Paginate(database.DB, &models.Product{}, page))
 }
 
 func CreateProduct(c *fiber.Ctx) error {
