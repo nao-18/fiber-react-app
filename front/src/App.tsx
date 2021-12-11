b922945d94a38c7e8758c90f0a9a95e3bd185560
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-11 22:25:13 +0900

    added table

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,6 +12,31 @@ function App() {
       <div className="container-fluid">
         <div className="row">
           <Menu />
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="table-responsive">
+              <table className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>Lorem</td>
+                    <td>ipsum</td>
+                    <td>dolor</td>
+                    <td>sit</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
         </div>
       </div>
     </div>
