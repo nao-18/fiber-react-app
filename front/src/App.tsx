3cd37c93f3518798852cce6f8791110d29041d4b
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-12 19:04:09 +0900

    added a path to the dashboard and user screens

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
 
 function App() {
   return (
@@ -15,7 +17,10 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <Dashboard />
+            <BrowserRouter>
+              <Route path={'/'} component={Dashboard} />
+              <Route path={'/users'} component={Users} />
+            </BrowserRouter>
           </main>
         </div>
       </div>
