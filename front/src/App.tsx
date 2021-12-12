ae2f44ea91ae01268958ede2a920f479752c94c7
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-12 18:43:37 +0900

    added page components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -14,28 +15,7 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <Dashboard />
           </main>
         </div>
       </div>
